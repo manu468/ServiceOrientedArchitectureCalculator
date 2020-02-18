432b825764b1bf40348a2381ca456a8a79b650c9
manu468 <manojmuppavarapu@gmail.com>
2020-02-17 19:47:09 -0500

    Add project files.

---
+++ MyWebService/App_Code/WebService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    //    [WebMethod]
+    //    public string HelloWorld()
+    //    {
+    //        return "Hello World";
+    //    }
+
+    //}
+    [WebMethod]
+    //Attaching the WebMethod attribute to a Public method indicates that you want the method exposed as part of the
+    //XML Web service
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    [WebMethod]
+    public int Sub(int x, int y)
+    {
+        return x - y;
+    }
+    [WebMethod]
+    public int Mul(int x, int y)
+    {
+        return x * y;
+    }
+    [WebMethod]
+    public int Div(int x, int y)
+    {
+        return x / y;
+    }
+
+}
